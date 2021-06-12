0ace8642067856a3143b77f3594a0bfd53ac7958
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-06-12 09:22:00 -0400

    Added react gems + ActualTime component

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,15 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-// import React from "react"
-// import ReactDOM from "react-dom"
-
-// ReactDOM.render(<App />, document.getElementById("actual-time"))
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
